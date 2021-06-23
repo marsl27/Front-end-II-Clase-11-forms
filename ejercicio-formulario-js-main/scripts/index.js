8e78eb64efa5052b35393c9cb56135126935b5dc
marsl27 <marielceciliaromeo@hotmail.com>
2021-06-23 10:43:26 -0300

    Le di formato

--- ejercicio-formulario-js-main/scripts/index.js
+++ ejercicio-formulario-js-main/scripts/index.js
@@ -3,10 +3,15 @@ const registerBtn = document.querySelector(".register-btn");
 registerBtn.addEventListener("click", () => {
   // Escribe tu código aquí, siguiendo los siguientes lineamientos paso a paso:
   // 1. Obtenemos el valor ingresado en el input de email
+
   let email = document.querySelector("#email-input").value;
+
   // 2. Obtenemos los datos ingresados en el input de password
+
   let password = document.querySelector("#password-input").value;
+
   // 3. Obtenemos el valor del input radio
+
   let elementos = document.getElementsByName("legalAge");
   let opcionesRadio = [];
   let seleccionesRadio = [];
@@ -14,12 +19,16 @@ registerBtn.addEventListener("click", () => {
     opcionesRadio[i] = elemento.value;
     seleccionesRadio[i] = elemento.checked;
   })
+
   // 4. Obtenemos el valor del input checkbox
+
   let check = document.querySelector("#tyc-input");
   let opcionCheck = check.value;
   let seleccionCheck = check.checked;
+
   // 5 Validamos si el usuario es mayor de edad. Si no, mostramos
   // un mensaje de error: "Debes ser mayor de edad para registrarte en el sitio"
+  
   let form = document.querySelector("form");
   function validarMayorEdad(arrayRadio) {
     if (arrayRadio[1] == true) {
